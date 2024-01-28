bce0615c0653b593e89697bdd0d4450e3f09122c
Fetozo <91467223+Fetozo@users.noreply.github.com>
2024-01-28 18:00:05 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Threading;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите первое число: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 == num2)
+        {
+            Console.WriteLine("Числа равны.");
+        }
+        else if (num1 > num2)
+        {
+            Console.WriteLine("Первое число больше второго.");
+        }
+        else
+        {
+            Console.WriteLine("Первое число меньше второго.");
+        }
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
