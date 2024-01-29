044a17fc12648acbb88aab386f19072a9fdd53f1
Fetozo <91467223+Fetozo@users.noreply.github.com>
2024-01-29 16:14:07 +0300

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task8
+{
+    class task8
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.Write($"{i * j}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
